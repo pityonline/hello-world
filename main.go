e20c2471452bc4a0bb0742d57b62c3dd2447ddf1
pityonline <pityonline@gmail.com>
2018-05-07 09:14:46 +0800

    add say func

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+// say prints something
+func say(s string) string {
+	return s
+}
+
 func main() {
-	fmt.Println("Hello world.")
+	fmt.Println(say("Hello world!"))
 }
